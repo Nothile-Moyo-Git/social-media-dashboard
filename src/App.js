25c9a239f145985f2666fba149eb4b0896805601
Hensetsu <nothile1@gmail.com>
2022-05-24 10:11:14 +0100

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2 className="text-3xl font-bold"> Hello world </h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
